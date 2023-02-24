2fa5fc8f98e61f4177b1604ab828859b99b94ce4
sickSamurai <ldmckkb@gmail.com>
2023-02-24 17:08:30 -0500

    v1 finished

--- src/app/admin/pages/results-page/results-page.component.ts
+++ src/app/admin/pages/results-page/results-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnDestroy } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
 import { Subscription } from 'rxjs'
+import { VotingDTO } from 'src/app/models/VotingDTO'
 import { VotingService } from 'src/app/services/voting.service'
 
 import { VotingStatus } from '../../../models/VotingStatus'
@@ -11,7 +13,12 @@ import { VotingStatus } from '../../../models/VotingStatus'
 })
 export class ResultsPageComponent implements OnDestroy {
   votingStatus: VotingStatus
+  votingList: VotingDTO[]
+  selectedVoting?: VotingDTO
+  selectedVotingFormControl: FormControl
   votingStatusSubscription: Subscription
+  votingListSubscription: Subscription
+  selectedVotingFormControlSubscription: Subscription
 
   setStatus = (status: VotingStatus) => (this.votingStatus = status)
 
@@ -19,13 +26,34 @@ export class ResultsPageComponent implements OnDestroy {
     this.votingStatusSubscription = this.votingService.getVotingStatus().subscribe(this.setStatus)
   }
 
+  subscribeToVotingList() {
+    this.votingListSubscription = this.votingService.getVotingList().subscribe(votingList => {
+      this.selectedVoting = votingList[0]
+      this.votingList = votingList
+    })
+  }
+
+  subscribeToSelectedVotingFormControl() {
+    this.selectedVotingFormControlSubscription = this.selectedVotingFormControl.valueChanges.subscribe({
+      next: value => (this.selectedVoting = value)
+    })
+  }
+
   ngOnDestroy(): void {
     this.votingStatusSubscription.unsubscribe()
+    this.votingListSubscription.unsubscribe()
+    this.selectedVotingFormControlSubscription.unsubscribe()
   }
 
   constructor(private votingService: VotingService) {
     this.votingStatus = 'undefined'
+    this.votingList = []
+    this.selectedVotingFormControl = new FormControl('', [Validators.required])
     this.votingStatusSubscription = new Subscription()
+    this.votingListSubscription = new Subscription()
+    this.selectedVotingFormControlSubscription = new Subscription()
     this.subscribeToVotingStatus()
+    this.subscribeToVotingList()
+    this.subscribeToSelectedVotingFormControl()
   }
 }
